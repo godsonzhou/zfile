64508531fffc4f447b6a8150730e3ba7b997a08e
godsonzhou <godsonzhou@qq.com>
2025-04-15 16:48:19 +0800

    3

--- src/zfile/vfs/Operations/WcxArchive/WcxArchiveCopyInOperation.cs
+++ src/zfile/vfs/Operations/WcxArchive/WcxArchiveCopyInOperation.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-
+using Zfile.Operations;
 namespace ZFile.FileSources.WcxArchive
 {
     public class WcxArchiveCopyInOperation : ArchiveCopyInOperation
--- src/zfile/vfs/Operations/WcxArchive/WcxArchiveCopyOutOperation.cs
+++ src/zfile/vfs/Operations/WcxArchive/WcxArchiveCopyOutOperation.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
-
+using Zfile.Operations;
 namespace Files.FileSources.WcxArchive
 {
     public class WcxArchiveCopyOutOperation : ArchiveCopyOutOperation

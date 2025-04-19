5421dcd2787399519e041cc8b648a13d687bf79d
godsonzhou <godsonzhou@qq.com>
2025-04-19 16:48:17 +0800

    42

--- src/zfile/vfs/FileSources/WcxArchive/WcxArchiveCalcStatisticsOperation.cs
+++ src/zfile/vfs/FileSources/WcxArchive/WcxArchiveCalcStatisticsOperation.cs
@@ -5,10 +5,10 @@ namespace zfile
     /// <summary>
     /// Operation for calculating statistics of files in a WCX archive
     /// </summary>
-    public class WcxArchiveCalcStatisticsOperation : FileSourceOperation
+    public class WcxArchiveCalcStatisticsOperation : FileSourceCalcStatisticsOperation
     {
         private readonly IWcxArchiveFileSource _fileSource;
-        private readonly FileEntries _files;
+        private FileSourceCalcStatisticsOperationStatistics _statistics;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WcxArchiveCalcStatisticsOperation"/> class
@@ -16,35 +16,71 @@ namespace zfile
         /// <param name="fileSource">The file source</param>
         /// <param name="files">The files to calculate statistics for</param>
         public WcxArchiveCalcStatisticsOperation(IWcxArchiveFileSource fileSource, FileEntries files)
-            : base(fileSource)
+            : base(fileSource, files)
         {
             _fileSource = fileSource;
-            _files = files;
-            OperationType = FileSourceOperationType.CalcStatistics;
         }
 
         /// <summary>
-        /// Executes the operation
+        /// Initializes the operation
         /// </summary>
-        protected override void Execute()
+        protected override void Initialize()
+        {
+            // Get initialized statistics; then we change only what is needed
+            _statistics = RetrieveStatistics();
+        }
+
+        /// <summary>
+        /// Executes the main operation
+        /// </summary>
+        protected override void MainExecute()
         {
             // Implementation of statistics calculation
-            // This is a placeholder implementation
-            long totalSize = 0;
-            long totalCompressedSize = 0;
-
-            foreach (var file in _files)
+            for (int i = 0; i < Files.Count; i++)
             {
-                totalSize += file.Size;
-                totalCompressedSize += file.CompressedSize;
+                ProcessFile(Files[i]);
+                CheckOperationState();
+            }
+        }
+
+        /// <summary>
+        /// Process a single file for statistics
+        /// </summary>
+        /// <param name="file">The file to process</param>
+        private void ProcessFile(FileEntry file)
+        {
+            _statistics.CurrentFile = file.FullName;
+            UpdateStatistics(_statistics);
+
+            // Update statistics based on file type
+            if (file.IsDirectory)
+            {
+                _statistics.Directories++;
+                // Process subdirectories if needed
+                // ProcessSubDirs(file.FullName);
+            }
+            else if (file.IsLink)
+            {
+                _statistics.Links++;
+            }
+            else
+            {
+                _statistics.Files++;
+                _statistics.Size += file.Size;
+                _statistics.CompressedSize += file.CompressedSize;
+
+                // Update oldest/newest file information if available
+                if (file.ModificationTime != DateTime.MinValue)
+                {
+                    if (file.ModificationTime < _statistics.OldestFile || _statistics.OldestFile == DateTime.MaxValue)
+                        _statistics.OldestFile = file.ModificationTime;
+
+                    if (file.ModificationTime > _statistics.NewestFile)
+                        _statistics.NewestFile = file.ModificationTime;
+                }
             }
 
-            // Set the result
-            SetResult(new FileSourceOperationResult
-            {
-                Success = true,
-                Message = $"Total size: {totalSize} bytes, Compressed size: {totalCompressedSize} bytes"
-            });
+            UpdateStatistics(_statistics);
         }
     }
 }

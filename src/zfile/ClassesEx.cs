040e3df6ded571f0ad9562d1cf76226dd1f6fbbb
godsonzhou <godsonzhou@qq.com>
2025-04-19 21:55:36 +0800

    64

--- src/zfile/ClassesEx.cs
+++ src/zfile/ClassesEx.cs
@@ -47,7 +47,7 @@ namespace zfile
         {
             // Save form properties
             SaveFormProperties();
-            
+
             // Save screen DPI
             _iniFile.WriteInteger(_iniSection, "Screen_PixelsPerInch", Screen.PrimaryScreen.LogicalDpi);
         }
@@ -58,7 +58,7 @@ namespace zfile
             {
                 // Read screen DPI
                 _pixelsPerInch = _iniFile.ReadInteger(_iniSection, "Screen_PixelsPerInch", Screen.PrimaryScreen.LogicalDpi);
-                
+
                 // Restore form properties
                 RestoreFormProperties();
             }
@@ -175,8 +175,8 @@ namespace zfile
     /// <summary>
     /// Thread-safe object list
     /// </summary>
-    public class ThreadSafeList<T> where T : class,  IDisposable, IEnumerable<T>
-	{
+    public class ThreadSafeList<T> where T : class, IDisposable
+    {
         private List<T> _list;
         private object _lock;
 
@@ -220,9 +220,9 @@ namespace zfile
                 _list.Add(item);
                 return _list.Count - 1;
             }
-        }		
-	
-		public T this[int index]
+        }
+
+        public T this[int index]
         {
             get
             {
@@ -239,8 +239,8 @@ namespace zfile
                 }
             }
         }
-	
-		public int Count
+
+        public int Count
         {
             get
             {
@@ -250,8 +250,8 @@ namespace zfile
                 }
             }
         }
-	
-		public bool Contains(T item)
+
+        public bool Contains(T item)
         {
             lock (_lock)
             {
@@ -282,38 +282,38 @@ namespace zfile
                 return new List<T>(_list);
             }
         }
-	
-		internal void Dispose()
-		{
-			lock (_lock)
-			{
-				_list.Clear();
-			}
-		}
-	
-		internal IEnumerable<T> Clone()
-		{
-			throw new NotImplementedException();
-		}
-		/// <summary>
-		/// Returns an enumerator that iterates through the list
-		/// </summary>
-		/// <returns>An enumerator for the list</returns>
-		public IEnumerator<T> GetEnumerator()
-		{
-			List<T> snapshot;
-			lock (_lock)
-			{
-				snapshot = new List<T>(_list);
-			}
-			return snapshot.GetEnumerator();
-		}
-	}
-	
-	/// <summary>
-	/// Helper class for SynEdit control
-	/// </summary>
-	public static class SynEditHelper
+
+        internal void Dispose()
+        {
+            lock (_lock)
+            {
+                _list.Clear();
+            }
+        }
+
+        internal IEnumerable<T> Clone()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Returns an enumerator that iterates through the list
+        /// </summary>
+        /// <returns>An enumerator for the list</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            List<T> snapshot;
+            lock (_lock)
+            {
+                snapshot = new List<T>(_list);
+            }
+            return snapshot.GetEnumerator();
+        }
+    }
+
+    /// <summary>
+    /// Helper class for SynEdit control
+    /// </summary>
+    public static class SynEditHelper
     {
         public static void FixDefaultKeystrokes(this SynEdit synEdit)
         {
--- src/zfile/vfs/FileSources/WcxArchive/WcxHeader.cs
+++ src/zfile/vfs/FileSources/WcxArchive/WcxHeader.cs
@@ -1,34 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace zfile
 {
-    public class WcxHeader
-    {
+	public class WcxHeader : IDisposable, IEnumerable<WcxHeader>
+	{
 		public string ArcName { get; set; }
-        public string FileName { get; set; }
+		public string FileName { get; set; }
 		public int Flags { get; set; }
 		public int HostOS { get; set; }
 		public int UnpVer { get; set; }
 		public long PackSize { get; set; }
-        public long UnpSize { get; set; }
-        public FileAttributes FileAttr { get; set; }
-        public int FileTime { get; set; }
-        public int CRC { get; set; }
-        public int Method { get; set; }
+		public long UnpSize { get; set; }
+		public FileAttributes FileAttr { get; set; }
+		public int FileTime { get; set; }
+		public int CRC { get; set; }
+		public int Method { get; set; }
 		public string Cmt { get; set; }
 		public int CmtState { get; set; }
 
 		public bool IsDirectory => (FileAttr & FileAttributes.Directory) == FileAttributes.Directory;
 
-        public WcxHeader Clone()
-        {
-            return new WcxHeader
-            {
-                FileName = FileName,
-                PackSize = PackSize,
-                UnpSize = UnpSize,
-                FileAttr = FileAttr,
-                FileTime = FileTime,
-                CRC = CRC,
-                Method = Method,
+		public WcxHeader Clone()
+		{
+			return new WcxHeader
+			{
+				FileName = FileName,
+				PackSize = PackSize,
+				UnpSize = UnpSize,
+				FileAttr = FileAttr,
+				FileTime = FileTime,
+				CRC = CRC,
+				Method = Method,
 				Cmt = Cmt,
 				CmtState = CmtState,
 				ArcName = ArcName,
@@ -36,7 +40,7 @@ namespace zfile
 				HostOS = HostOS,
 				UnpVer = UnpVer
 			};
-        }
+		}
 		public WcxHeader() { }
 		public WcxHeader(THeaderData headerData)
 		{
@@ -70,7 +74,25 @@ namespace zfile
 			HostOS = headerDataExW.HostOS;
 			UnpVer = headerDataExW.UnpVer;
 		}
-	
-	
+
+		// Implement IDisposable
+		public void Dispose()
+		{
+			// No unmanaged resources to dispose
+			GC.SuppressFinalize(this);
+		}
+
+		// Implement IEnumerable<WcxHeader>
+		public IEnumerator<WcxHeader> GetEnumerator()
+		{
+			// Return only this instance in the enumeration
+			yield return this;
+		}
+
+		// Implement IEnumerable
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
 	}
 }

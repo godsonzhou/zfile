a72d71705c817168c1cb4039f8387cbef57f182f
godsonzhou <godsonzhou@qq.com>
2025-04-19 20:42:40 +0800

    56

--- src/zfile/vfs/Operations/ShellCalcStatisticsOperation.cs
+++ src/zfile/vfs/Operations/ShellCalcStatisticsOperation.cs
@@ -94,7 +94,7 @@ namespace zfile
                         }
                         else
                         {
-                            long size = GetDetails(folder, pidl, SCIDHelper.SCID_FileSize);
+                            long size = GetDetails(folder, pidl, SCIDHelper.FileSize);
                             statistics.Size += size;
                             statistics.Files++;
                         }

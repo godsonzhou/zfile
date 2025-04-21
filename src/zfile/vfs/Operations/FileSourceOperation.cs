431d790aa74796e522cb6308ea9a85e6d6173e72
godsonzhou <godsonzhou@qq.com>
2025-04-21 21:54:53 +0800

    move filesourceoperationextensions.cs to filesourceoperation.cs

--- src/zfile/vfs/Operations/FileSourceOperation.cs
+++ src/zfile/vfs/Operations/FileSourceOperation.cs
@@ -1001,4 +1001,21 @@ namespace zfile
     {
         FileSourceOperationUIResponse AskQuestion(string uiMessage, string uiQuestion, FileSourceOperationUIResponse[] uiPossibleResponses, FileSourceOperationUIResponse uiDefaultOKResponse, FileSourceOperationUIResponse uiDefaultCancelResponse, IFileSourceOperationUIActionHandler uiActionHandler);
     }
+
+	/// <summary>
+	/// 提供文件源操作的扩展方法
+	/// </summary>
+	public static class FileSourceOperationExtensions
+	{
+		/// <summary>
+		/// 创建一个包装委托，用于处理带有 actionHandler 参数的 AskQuestion 方法
+		/// </summary>
+		/// <param name="operation">文件源操作</param>
+		/// <returns>包装后的委托</returns>
+		public static AskQuestionFunction CreateAskQuestionDelegate(this FileSourceOperation operation)
+		{
+			return (msg, question, possibleResponses, defaultOKResponse, defaultCancelResponse) =>
+				operation.AskQuestion(msg, question, possibleResponses, defaultOKResponse, defaultCancelResponse, null);
+		}
+	}
 }

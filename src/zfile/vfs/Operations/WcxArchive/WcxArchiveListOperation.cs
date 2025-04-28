3b513413184fcff8665e8f20945081ea3affe5a9
godsonzhou <godsonzhou@qq.com>
2025-04-28 20:54:11 +0800

    r

--- src/zfile/vfs/Operations/WcxArchive/WcxArchiveListOperation.cs
+++ src/zfile/vfs/Operations/WcxArchive/WcxArchiveListOperation.cs
@@ -3,7 +3,7 @@ namespace zfile
 	public class WcxArchiveListOperation : FileSourceListOperation
 	{
 		private IWcxArchiveFileSource _wcxArchiveFileSource;
-		private bool _flatView;
+		//private bool _flatView;
 
 		public WcxArchiveListOperation(IFileSource fileSource, string path) : base(fileSource, path)
 		{

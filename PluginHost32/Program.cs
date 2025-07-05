fb28404d676f838c9b36a48541d00fffb3729b4c
godsonzhou <godsonzhou@qq.com>
2025-07-05 22:33:29 +0800

    1

--- PluginHost32/Program.cs
+++ PluginHost32/Program.cs
@@ -8,16 +8,26 @@ namespace PluginHost32
 		///  The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			//接收2个argument, 用于初始化pluginhost
-			var arg = Environment.GetCommandLineArgs();
-			Debug.Print($"pluginhost32: arg0={arg[0]} arg1={arg[1]}");
-			
-			Application.Run(new PluginHost(arg[0], arg[1]));
+			try
+			{
+				// To customize application configuration such as set high DPI settings or default font,
+				// see https://aka.ms/applicationconfiguration.
+				ApplicationConfiguration.Initialize();
+				//接收2个argument, 用于初始化pluginhost
+				//var arg = Environment.GetCommandLineArgs();
+				Console.WriteLine($"pluginhost32: arg0={args[0]} arg1={args[1]}");
+
+				Application.Run(new PluginHost(args[0], args[1]));
+			}
+			catch (Exception ex)
+			{
+				// 捕获异常并显示错误信息
+				MessageBox.Show($"插件加载失败: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Debug.Print($"PluginHost32: {ex}");
+				Environment.Exit(1);
+			}
 		}
 	}
 }
